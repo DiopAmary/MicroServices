1d6c5978d47f6eb11294b19c1def42c518f72803
DiopAmary <amarydiop27@gmail.com>
2022-01-07 18:37:28 +0100

    cqrs - es commands implementation

---
+++ cqrs-es/src/main/java/ma/enset/cqrses/commonapi/commands/AccountDebitCommand.java
@@ -0,0 +1,16 @@
+package ma.enset.cqrses.commonapi.commands;
+
+import lombok.Getter;
+
+public class AccountDebitCommand extends BaseCommand<String>{
+    @Getter
+    private String currency;
+    @Getter
+    private double balance;
+
+    public AccountDebitCommand(String id, String currency, double balance) {
+        super(id);
+        this.currency = currency;
+        this.balance = balance;
+    }
+}
